8ea820282c027047dd0e7858904c285f7533a3a3
vishnu <vishnu.tvm777@gmail.com>
2023-06-13 14:27:41 +0530

    login_signup

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -3,7 +3,9 @@ from django.urls import  path
 from .import views
 
 urlpatterns = [
-    path('',views.add,name="add"),
+    path('',views.login,name="login"),
+    path('signup',views.signup,name="signup"),
+    path('index',views.add,name="add"),
     path('delete/<int:id>/', views.delete, name='delete'),
     path('update/<int:id>/', views.update, name='update')
    
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import crud
 from django.contrib import messages
+from django.contrib.auth.models import User,auth
 # Create your views here.
+
+def signup(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        if password1 != password2:
+            messages.error(request, "Passwords do not match")
+            return redirect('signup')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect('signup')
+            
+        user= User.objects.create_user(username=username,email=email,password=password1)
+        user.save();
+        print("user created")
+        return redirect('login')
+
+   
+    return render(request,'signup.html')
+    
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password1=request.POST['password1']
+        user= auth.authenticate(username=username,password=password1)
+        print(user)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('add')
+        else:
+            return render(request,'login.html')
+    return render(request,'login.html')
+
+
 def add(request):
     task1 = crud.objects.all()
     if request.method=='POST':
